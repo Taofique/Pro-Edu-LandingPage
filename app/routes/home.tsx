a5c5e8807ebc55c1ae483214e8788c482a623816
Taofique <shornob8@gmail.com>
2026-09-01 14:30:20 +0600

    chore: Initial folder,file and dependency setup for the Pro-Edu LandingPage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Pro Edu" },
+    { name: "description", content: "Welcome to Pro Edu Landing Page!" },
   ];
 }
 
